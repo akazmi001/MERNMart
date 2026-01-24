7c691919328f229d8bdfed853e5b45f79a23bc47
Mohammad Anas <anasm9038@gmail.com>
2026-01-24 15:12:41 +0530

    Password changed

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: 'mongodb+srv://akazmi001:Abida123@cluster0.ahojmbo.mongodb.net/mern-marketplace?retryWrites=true&w=majority',
+  mongoUri: process.env.MONGODB_URI,
   stripe_connect_test_client_id: 'YOUR_stripe_connect_test_client',
   stripe_test_secret_key: 'YOUR_stripe_test_secret_key',
   stripe_test_api_key: 'YOUR_stripe_test_api_key' 
